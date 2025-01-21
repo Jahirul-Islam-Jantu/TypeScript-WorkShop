152ca3e794bff4f8e366282f0353b02ece6169ad
Jahirul Islam <jahirulislamjantu002@gmail.com>
2025-01-21 22:21:12 +0600

    typescript workshop first lession structure build up

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let message: string = "Hello, World!";
+
+let number: number = 42;
+
+let isActive: boolean = true;
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+
+let messages: string[] = ["Hello", "World", "!"];
+
+let person: { name: string; age: number } = { name: "John Doe", age: 30 };
+
+console.log(person.name);
